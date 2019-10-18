63b75a598b831d50f5ac12120263275828c62752
Duong <dxa4k26@gmail.com>
2019-10-18 16:16:06 +0700

    add quicksort version char and double

--- src/main/java/devops/quicksort/Quicksort.java
+++ src/main/java/devops/quicksort/Quicksort.java
@@ -24,6 +24,48 @@ public class Quicksort {
 		if(i < r)
 			run(a, i, r);
 	}
-
-
+	
+	public void run(char a[], int l, int r) {
+		char key = a[(l + r)/2];
+		int i = l, j = r;
+		while(i <= j) {
+			while(a[i] < key)
+				i++;
+			while(a[j] > key)
+				j--;
+			if(i <= j) {
+				char temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		if(l < j)
+			run(a, l, j);
+		if(i < r)
+			run(a, i, r);
+	}
+	
+	public void run(double a[], int l, int r) {
+		double key = a[(l + r)/2];
+		int i = l, j = r;
+		while(i <= j) {
+			while(a[i] < key)
+				i++;
+			while(a[j] > key)
+				j--;
+			if(i <= j) {
+				double temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		if(l < j)
+			run(a, l, j);
+		if(i < r)
+			run(a, i, r);
+	}
 }
--- src/test/java/devops/quicksort/QuicksortTest.java
+++ src/test/java/devops/quicksort/QuicksortTest.java
@@ -18,4 +18,28 @@ public class QuicksortTest {
 		
 		assertArrayEquals(expected, test);
 	}
+	
+	@Test
+	public void successSortFloat() {
+
+		double[] expected = { 1.2, 2.3, 3.5, 4.1, 4.6, 6.0, 7.2 };
+		double[] test = { 1.2, 2.3, 4.6, 4.1, 6.0, 7.2, 3.5 };
+		
+		qs.run(test, 0, test.length - 1);
+		
+		assertArrayEquals(expected, test, 0);
+		
+	}
+	
+	@Test
+	public void successSortChar() {
+
+		char[] expected = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+		char[] test = { 'g', 'd', 'e', 'f', 'b', 'a', 'c' };
+		
+		qs.run(test, 0, test.length - 1);
+		
+		assertArrayEquals(expected, test);
+		
+	}
 }
